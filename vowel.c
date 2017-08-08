4e2b3808bb382717c2c600ab94c3b174a9b900b4
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2017-08-08 15:57:04 +0530

    c.c
    vc.c

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch==a||ch==A||ch==e||ch==i||ch==o||ch==u||ch==E||ch==I||ch==O||ch==U)
+{
+printf("%c is vowel",ch);
+}
+else
+{
+printf("%c is not vowel",ch);
+}
+return 0;
+}
+
