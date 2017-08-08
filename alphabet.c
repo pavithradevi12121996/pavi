5d7a8f192a25f3c55a15dec4ba24d3e5e6cb34fd
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2017-08-08 16:17:17 +0530

    a.c
    ff.c

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+printf("enter character");
+scanf("%d",&c);
+if((c>='a'&&c<='z')||(c>'A'&&(C>'Z'))
+{
+printf("%c is alphabet");
+}
+else
+{
+printf({"%c ic not alphabet");
+}
+return 0;
+}
