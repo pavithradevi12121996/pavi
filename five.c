00f28cc961eb4e4f9e09b6d4889bb84b3108308f
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-08 16:50:14 +0530

    five
    wdwdwd

---
+++ five.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r;
+printf("enter value for r");
+scanf("%d",&r);
+for(n=1;n<r;n++)
+{
+if(n%5==0)
+{
+printf("numbers are %d",n)
+}
+}
+getch();
+}
