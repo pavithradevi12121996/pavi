4eb2c99c0ae72de949cd7313fbb4ec27955a6ad2
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-10 16:39:21 +0530

    factorial.c
    fdf

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter value for n");
+scanf("%d",&n);
+if(n==0||n==1)
+{
+return 1;
+}
+else
+{
+ return factn*(n-1)
+ }
+ getch();
+ }
+ 
