4d879de30f8138508ae49af1134ce612f82c2655
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-10 16:08:35 +0530

    oddint.c

---
+++ oddint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b,i;
+clrscr();
+printf("enter numbers");
+scanf("%d",&b);
+for(i=1;i<b;i++)
+{
+if(i%2==1)
+{
+printf("the values are %d",i):
+}}
+getch();
+}
