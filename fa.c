c64e72f2d198e19da2f01f32e5039529b6a7e4f9
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-10 16:54:50 +0530

    fa.c
    gh

---
+++ fa.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int fact=0;
+printf("factorial of a number");
+scanf("%d",&n);
+if(n==0||n==1)
+{
+return 1;
+}
+else
+{
+n=n*(n-1);
+printf("fact is ",n);
+}
+return o;
+}
