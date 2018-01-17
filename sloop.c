e41ab779154f0429e9b147e6e98d7041eb332e11
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-17 15:48:33 +0530

    sloop.c
    eee

---
+++ sloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conoi.h>
+void main()
+{
+int i,sum=0;
+clrcsr();
+printf("enter value of n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+sum=sum+i;
+printf("sum is %d",sum)
+}
+getch();
+}
