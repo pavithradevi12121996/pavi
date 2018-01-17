00f88783226f1606a3f964f008eed388dee4ae98
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-17 15:53:21 +0530

    loop.c
    ew

---
+++ leap2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+printf("enter leap year");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf("year is leap");
+}
+else 
+{
+printf("year is not leap");
+}
+getch();
+}
