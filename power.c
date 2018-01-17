ed084706490f071ac100f5b3017288dea0f88db5
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-17 16:59:41 +0530

    power.c
    ass

--- power.c
+++ power.c
@@ -1,12 +1,13 @@
 #include<stdio.h> 
 int main()
 {
-int n,k,i
+int n,k,i;
 printf("enter two values");
-scanf("%d%d",&n&k);
-for(i=n;i<k;i++)
+scanf("%d%d",&n,&k);
+while(i<=k)
 {
-n=n*i;
+n=n*i
+  i++;
 }
 printf("power is %d",n);
 }
