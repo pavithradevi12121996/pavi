50174eeb78570ef309d0d9ce1eff47fbf0feba0c
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-22 16:35:47 +0530

    arraymax.c
    swd

---
+++ arraymax.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a[10],n;
+printf("enter no of element");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+printf(" \n array elements are %d",a[i]);
+}
+return 0;
+}
