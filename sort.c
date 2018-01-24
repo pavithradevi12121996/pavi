3fbb185dfc23e24fe580f056c0867f5461adc5d2
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-24 16:28:00 +0530

    sort.c

---
+++ sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,n;
+printf("enter array element");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("\narray %d",i);
+}
+int min=a[i],temp,j;
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(min<a[i])
+{
+	temp=a[i];
+	a[i]=a[j];
+	a[j]=temp;
+printf("%d",a[i]);
+}
+}
+}
+return 0;
+}
+
