9bf5016466519a03f73b7a914834a516a142368a
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-01-31 15:47:07 +0530

    arraymax.c

--- arraymax.c
+++ arraymax.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int a[10],n;
+int a[10],n,int max=a[0];
 printf("enter no of element");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -9,5 +9,9 @@ for(i=0;i<n;i++)
 scanf("%d",&a[i]);
 printf(" \n array elements are %d",a[i]);
 }
+  if(a[i]>max)
+  {
+    printf("maximum element are %d",a[i]);
+  }
 return 0;
 }
