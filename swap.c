b53ce2bdc1393c493b19076beb6db5cb222907ee
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-02 16:35:00 +0530

    swap.c

--- swap.c
+++ swap.c
@@ -4,9 +4,10 @@ int main()
 int n1,n2;
 printf("enter two numbers");
 scanf("%d%d",&n1,&n2);
+printf("\nbefore swap %d %d",n1,n2);
 n1=n1+n2;
 n2=n1-n2;
 n1=n1-n2;
-printf("%d%d",n1,n2);
+printf("\nafter swap %d %d",n1,n2);
 return 0;
 }
