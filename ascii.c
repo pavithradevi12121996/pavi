f9b14eea341258164318d2452e5d5045506f89b1
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-02 15:38:29 +0530

    ascii.c

---
+++ ascii.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i,sum=0;
+printf("enter number");
+scanf("%d",&n);
+printf("enter sequence");
+scanf("%d",&d)
+for(i=0;i<n;i+d)
+{
+sum=sum+i;
+}
+printf("sequence is %d",sum);
+return 0;
+}
