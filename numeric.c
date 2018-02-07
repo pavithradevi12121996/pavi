8de5a778ee3bd570b1e283550f1ba113e64a35ad
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-07 15:55:50 +0530

    numeric.c

---
+++ numeric.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	
+int a,count=0,i;
+printf("enter value of a");
+scanf("%d",&a);
+for(i=1;i<a;i++)
+{
+count++;
+}
+printf("\nvalue count is \n%d",count);
+return 0;
+}
