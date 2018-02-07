1c2a03f43ca031d08d83f9c68c8087a16da9b198
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-07 15:45:50 +0530

    swap.c

--- 2swap.c
+++ 2swap.c
@@ -1,11 +1,19 @@
-#include<stdo.h>
+#include<stdio.h>
+
 int main()
 {
 int a,b;
 printf("enter two marks");
 scanf("%d%d",&a,&b);
-swap(&a,&b);
-printf("after swap %d",a,b);
-return 0;
+void swap(a,b);
+printf("after swap %d\n%d",a,b);
 }
-
+void swap(a,b)
+{
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	printf("value of %d%d",a,b);
+	return 0;
+}
