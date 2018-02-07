ab9c9b5ef5e0f7b6c546f66ab541a138ac089aa0
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-07 16:17:39 +0530

    45.c

---
+++ 45.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,count=0,i;
+printf("enter number of digits");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+count++;
+printf("number of digits \n%d",count);
+}
+return 0;
+}
+
