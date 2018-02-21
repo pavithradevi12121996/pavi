be9f49f520075ce7c332efd59e90e0a86f317268
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-21 16:11:42 +0530

    68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter value of a");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("number is multiple of seven");
+}
+else
+{
+printf("not multiple of seven");
+}
+getch();
+}
