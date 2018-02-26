041bdb1eb61c7973f428eab9ba4cf1093f6a540d
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-02-26 16:19:37 +0530

    78.c

---
+++ 78.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter a number ");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("mul of 13");
+}
+else
+{
+printf("not mul of 13");
+}
+getch();
+}
+
