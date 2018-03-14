62edce0ec48527febe9425ce721636d608c23c2a
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-03-14 16:00:51 +0530

    96.c

---
+++ 96.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter number");
+scanf("%d",&n);
+if(n%n==0)
+{
+if(n/1==n)
+{
+printf("number is prime");
+}
+}
+else
+{
+printf("number is composite");
+}
+getch();
+}
