c91c7318d6478d85354df2e844e1e5be64ddf11c
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-03-14 16:39:50 +0530

    107.c

---
+++ 107.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+printf("enter a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+d=((a*b)/c);
+printf("value is %d",d);
+getch();
+}
