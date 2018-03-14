607fcf049d04e77a0e0bf1d35bc71709a6ccccec
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-03-14 15:57:44 +0530

    94.c

---
+++ 94.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],b,n,i;
+printf("enter n");
+scanf("%d",&n);
+printf("enter number");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("numbers are %d',a[i]);
+printf("enter b value");
+scanf("%d",&b);
+printf("a[b] is %d",a[i]);
+getch();
+}
