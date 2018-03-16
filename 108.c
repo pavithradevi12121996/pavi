4dd9c931a556357f43855ec35d5b6905652f36b7
pavithradevi12121996 <30549705+pavithradevi12121996@users.noreply.github.com>
2018-03-16 15:44:57 +0530

    108.c

---
+++ 108.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,sum=0;
+printf("enter a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+d=(b/a);
+e=(c/b);
+sum=(d+e);
+printf("sum ix %d",sum);
+getch();
+}
+
+
